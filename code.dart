ce9a2704f0a1c61dc4b888c540b7d63dfd11aa50
Maimoona M.Irfan <87384658+maimoonairfan@users.noreply.github.com>
2024-03-04 15:59:25 +0500

    Add files via upload

---
+++ code.dart
@@ -0,0 +1,33 @@
+//Selection Sort
+// void main() {
+//   List num = [ 9,0,8,7,5];
+//   List new_num = [...num];
+//   List sortedList = [];
+//   while (new_num.length > 0) {
+//     var min = new_num[0];
+//     for (var i in new_num) {
+//       if (min > i) {
+//         min=i;
+//       }
+//     }
+//     sortedList.add(min);
+//     new_num.remove(min);
+//   }
+//   print(num);
+//   print(sortedList);
+// }
+//Occurence Count
+// void main() {
+//   List<int> num_list = [2, 8, 9, 2, 2,8, 9];
+//   Map Count = {};
+//   for (int num in num_list) {
+//     if (Count.containsKey(num)) {
+//       Count[num] = Count[num] + 1;
+//     } else {
+//       Count[num] = 1;
+//     }
+//   }
+//   Count.forEach(
+//     (key, value) => print('Element $key Count $value'),
+//   );
+// }
